aea9aca9adf28689f8a6ae08104afa2041f527fd
ankiit-yadavv <ankit9319421847@gmail.com>
2023-10-25 20:28:13 +0530

    Created private profile page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -39,9 +39,7 @@ export default function SignIn() {
       } catch (error) {
         dispatch(signInFailure(data.message));        
       }
-    // console.log(data);
   };
-  // console.log(formData);
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
