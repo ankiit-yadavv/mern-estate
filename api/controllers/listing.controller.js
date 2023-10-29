d1438edd86a71030f0266b9edb6825629034c1a5
ankiit-yadavv <ankit9319421847@gmail.com>
2023-10-29 22:58:16 +0530

    created update listing API

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -20,11 +20,32 @@ export const deleteListing = async (req, res, next) => {
     }
 
     if(req.user.id !== listing.userRef){
-        return next(errorHandler(401, 'You can only delete our listing'));
+        return next(errorHandler(401, 'You can only delete your listing only!'));
     }
     try {
         await Listing.findByIdAndDelete(req.params.id);
         res.status(200).json('Listing has been deleted successfully');        
+    } catch (error) {
+        next(error);
+    }
+};
+
+
+export const updateListing = async (req, res, next) => {
+    const listing = await Listing.findById(req.params.id);
+    if(!listing) {
+        return next(errorHandler(404, 'Lisiting not found'));
+    }
+    if(req.user.id !== listing.userRef){
+        return next(errorHandler(401, 'You can only update your listing only!'));
+    }
+    try {
+        const updatedListing = await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new: true}
+        ); 
+        res.status(200).json(updatedListing); 
     } catch (error) {
         next(error);
     }
